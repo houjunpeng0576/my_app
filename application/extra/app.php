db94100811870b8e5eea63db331d5057699d06a4
houjunpeng0576 <1643394646@qq.com>
2018-03-05 13:29:49 +0800

    删除信息3

--- application/extra/app.php
+++ application/extra/app.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'password_pre_halt' => '_#myapp_blog',//密码加密盐
-    'aes_key' => 'hou951229',//aes加密盐，客户端和服务端必须保持一致
+    'password_pre_halt' => '',//密码加密盐
+    'aes_key' => '',//aes加密盐，客户端和服务端必须保持一致
     'app_types' => [
         'ios',
         'android',
     ],//设备类型
-    'app_sign_time' => 10,//sign失效时间(用于时间有效性)111
+    'app_sign_time' => 10,//sign失效时间(用于时间有效性)
     'app_sign_cache_time' => 20,//sign缓存失效时间(用于时间唯一性)
 ];
--- application/extra/cat.php
+++ application/extra/cat.php
@@ -23,3 +23,13 @@ return [
         '19' => 'IT'
     ]
 ];
+return [
+    'password_pre_halt' => '_#myapp_blog',//密码加密盐
+    'aes_key' => 'hou951229',//aes加密盐，客户端和服务端必须保持一致
+    'app_types' => [
+        'ios',
+        'android',
+    ],//设备类型
+    'app_sign_time' => 10,//sign失效时间(用于时间有效性)
+    'app_sign_cache_time' => 20,//sign缓存失效时间(用于时间唯一性)
+];
